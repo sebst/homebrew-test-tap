a24d9e79de5ad4030b4ff7527093aa533b5fded3
Sebastian Steins <592313+sebst@users.noreply.github.com>
2024-08-02 18:20:48 -0500

    ........

--- Formula/gothanks2.rb
+++ Formula/gothanks2.rb
@@ -1,5 +1,5 @@
 class Gothanks2 < Formula
-    desc "Automatically star your go.mod Github dependenciels"
+    desc "Automatically star your go.mod Github dependencies"
     homepage "https://github.com/psampaz/gothanks"
     url "https://github.com/psampaz/gothanks/archive/refs/tags/v0.5.0.tar.gz"
     sha256 "932915b470a3c55edf6b6524d0062a762ae79c2c9b39685aa1a53aae53449265"
@@ -17,4 +17,3 @@ class Gothanks2 < Formula
       assert_match "no Github token found", shell_output(bin/"gothanks", 255)
     end
   end
-  
