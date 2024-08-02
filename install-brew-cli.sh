1109b14d4ca039d3cda031b70dedb19c74470d7d
Sebastian Steins <592313+sebst@users.noreply.github.com>
2024-08-02 15:31:58 -0500

    Install brew

--- install-brew-cli.sh
+++ install-brew-cli.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get install -y --no-install-recommends \
+    build-essential \
+    curl \
+    file \
+    git \
+    sudo \
+    unzip \
+    wget \
+    xz-utils
+
 /bin/bash -c "$(cat install-brew.sh)"
