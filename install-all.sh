9b7214bb699b1991fc750549d9c6e220e628e55a
Sebastian Steins <592313+sebst@users.noreply.github.com>
2024-08-02 16:44:30 -0500

    install-helper

--- install-all.sh
+++ install-all.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 
+if command -v brew &> /dev/null
+then
+    echo "brew is already installed"
+    exit 0
+fi
+
 ./install-01-dependencies.sh
 ./install-02-ruby.sh
 ./install-03-brew.sh
