f84aa2d0838982861372fc257398774110db319f
Sebastian Steins <592313+sebst@users.noreply.github.com>
2024-08-02 15:07:57 -0500

    gothanks2

---
+++ Formula/gothanks2.rb
@@ -0,0 +1,20 @@
+class Gothanks2 < Formula
+    desc "Automatically star your go.mod Github dependencies"
+    homepage "https://github.com/psampaz/gothanks"
+    url "https://github.com/psampaz/gothanks/archive/refs/tags/v0.5.0.tar.gz"
+    sha256 "932915b470a3c55edf6b6524d0062a762ae79c2c9b39685aa1a53aae53449265"
+    license "MIT"
+    head "https://github.com/psampaz/gothanks.git", branch: "master"
+  
+    depends_on "go" => :build
+  
+    def install
+      system "go", "build", *std_go_args(ldflags: "-s -w")
+    end
+  
+    test do
+      ENV.delete "GITHUB_TOKEN"
+      assert_match "no Github token found", shell_output(bin/"gothanks", 255)
+    end
+  end
+  
