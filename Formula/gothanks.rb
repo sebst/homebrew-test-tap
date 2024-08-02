7d96aa71cadf4982f9e070d8bc0e700b1c461fb2
Sebastian Steins <592313+sebst@users.noreply.github.com>
2024-08-02 13:14:26 -0500

    gothanks 0.3.0 (edited formula 3)

--- Formula/gothanks.rb
+++ Formula/gothanks.rb
@@ -1,14 +1,14 @@
 class Gothanks < Formula
-  desc "Automatically star your go.mod Github dependencies"
-  homepage "https://github.com/psampaz/gothanks"
-  url "https://github.com/psampaz/gothanks/archive/v0.3.0.tar.gz"
-  sha256 "ce5440334b3eac2e058724faa4c6e4478ca1d81ea087e55ccca33f1996752aad"
+  desc "GoThanks automatically stars Go's official repository and your go.mod github dependencies, providing a simple way  to say thanks to the maintainers of the modules you use and the contributors of Go itself."
+  homepage ""
+  url "https://github.com/psampaz/gothanks/archive/v0.5.0.tar.gz"
+  sha256 "932915b470a3c55edf6b6524d0062a762ae79c2c9b39685aa1a53aae53449265"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   test do
