54735e2b2e08b760da05662faad0ca1b05559de4
Sebastian Steins <592313+sebst@users.noreply.github.com>
2024-08-02 12:56:29 -0500

    gothanks 0.3.0 (edited formula)

--- Formula/gothanks.rb
+++ Formula/gothanks.rb
@@ -1,9 +1,6 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Gothanks < Formula
-  desc "GoThanks automatically stars Go's official repository and your go.mod github dependencies, providing a simple way  to say thanks to the maintainers of the modules you use and the contributors of Go itself."
-  homepage ""
+  desc "Automatically star your go.mod Github dependencies"
+  homepage "https://github.com/psampaz/gothanks"
   url "https://github.com/psampaz/gothanks/archive/v0.3.0.tar.gz"
   sha256 "ce5440334b3eac2e058724faa4c6e4478ca1d81ea087e55ccca33f1996752aad"
   license "MIT"
@@ -11,19 +8,11 @@ class Gothanks < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+    system "go", "build", *std_go_args
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test gothanks`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    ENV.delete "GITHUB_TOKEN"
+    assert_match "no Github token found", shell_output(bin/"gothanks", 255)
   end
 end
