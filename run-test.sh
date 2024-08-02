39177d6346f27f188002ea953ecd374198f6bb8b
Sebastian Steins <592313+sebst@users.noreply.github.com>
2024-08-02 17:22:04 -0500

    ........

--- run-test.sh
+++ run-test.sh
@@ -3,8 +3,11 @@
 export ASDF_DIR=/home/ubuntu/.asdf
 export PATH=/home/linuxbrew/.linuxbrew/bin:/home/ubuntu/.asdf/shims:/home/ubuntu/.asdf/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
+brew --version
+ruby --version
 
-/home/linuxbrew/.linuxbrew/bin/brew test-bot --only-cleanup-before
-/home/linuxbrew/.linuxbrew/bin/brew test-bot --only-setup
-/home/linuxbrew/.linuxbrew/bin/brew test-bot --only-tap-syntax
-# /home/linuxbrew/.linuxbrew/bin/brew test-bot --only-formulae
+
+# /home/linuxbrew/.linuxbrew/bin/brew test-bot --only-cleanup-before
+# /home/linuxbrew/.linuxbrew/bin/brew test-bot --only-setup
+# /home/linuxbrew/.linuxbrew/bin/brew test-bot --only-tap-syntax
+# # /home/linuxbrew/.linuxbrew/bin/brew test-bot --only-formulae
