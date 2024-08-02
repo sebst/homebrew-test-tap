ee464448cd392966b0e0a9139c140f3aab3e8938
Sebastian Steins <592313+sebst@users.noreply.github.com>
2024-08-02 16:19:06 -0500

    ...,..

--- install-02-ruby.sh
+++ install-02-ruby.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-
-asdf plugin add ruby
-asdf install ruby 3.3.4
-asdf global ruby 3.3.4
+source ~/.bashrc
+/home/ubuntu/.asdf/bin/asdf plugin add ruby
+/home/ubuntu/.asdf/bin/asdf install ruby 3.3.4
+/home/ubuntu/.asdf/bin/asdf global ruby 3.3.4
 gem update --system
 echo "::endgroup::"
 
